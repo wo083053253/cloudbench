7c248db2d8369f11677b1f241fad57deeb551223
Thomas Orozco <thomas@orozco.fr>
2013-07-17 10:17:00 -0700

    Add a setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='fio',
+    version='0.1',
+    packages=['fio', 'fio.test'],
+    url='',
+    license='',
+    author='Thomas Orozco',
+    author_email='thomas@scalr.com',
+    description='FIO Wrapper'
+)
