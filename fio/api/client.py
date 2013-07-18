a8477de85ba1d8a17e4b31d16accacb59259ff64
Thomas Orozco <thomas@orozco.fr>
2013-07-17 18:52:09 -0700

    Add create method to measurements

--- fio/api/client.py
+++ fio/api/client.py
@@ -19,7 +19,10 @@ class Client(object):
     def base_api_path(self):
         return path_join(self._api_path, self._api_version)
 
-    list_measurements = make_list_method("measurement")
     list_instances = make_list_method("instance")
+
+    list_measurements = make_list_method("measurement")
+    create_measurement = make_create_method("measurement")
+
     list_datapoints = make_list_method("datapoint")
     create_datapoint = make_create_method("datapoint")
