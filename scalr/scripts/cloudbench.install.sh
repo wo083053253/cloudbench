83943c5911fe65226607ae87321f1698c93a78f8
Thomas Orozco <thomas@orozco.fr>
2014-05-29 15:39:05 +0200

    Don't install Fio if already installed

--- scalr/scripts/cloudbench.install.sh
+++ scalr/scripts/cloudbench.install.sh
@@ -8,13 +8,18 @@ apt-get install -y python-setuptools python-pip curl bzip2 build-essential libai
 pip uninstall --yes python-novaclient || true
 pip install lockfile boto pyrax cloudbench
 
-FIO_BUILD=/tmp/$$-build/
+function install_fio {
+  FIO_BUILD=/tmp/$$-build/
+  mkdir -p $FIO_BUILD
+  cd $FIO_BUILD
+  curl --silent http://brick.kernel.dk/snaps/fio-2.0.15.tar.bz2 > fio-2.0.15.tar.bz2
+  bunzip2 fio-2.0.15.tar.bz2
+  tar -xf fio-2.0.15.tar
+  cd fio-2.0.15
+  make
+  make install
+}
 
-mkdir -p $FIO_BUILD
-cd $FIO_BUILD
-curl --silent http://brick.kernel.dk/snaps/fio-2.0.15.tar.bz2 > fio-2.0.15.tar.bz2
-bunzip2 fio-2.0.15.tar.bz2
-tar -xf fio-2.0.15.tar
-cd fio-2.0.15
-make
-make install
+fio -v | grep -E "2\.0\.15" || install_fio
+
+exit 0
